440f565425923f42c65aad9103baf4f3ad4ef6c9
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-04-26 17:57:52 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -40,7 +40,7 @@ const BuscaCepIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'BuscaCepIntent';
     },
     handle(handlerInput) {
-        const cep = handlerInput.requestEnvelope.request.intent.slots.cep.value.toString();
+        const cep = handlerInput.requestEnvelope.request.intent.slots.cep.value;
         
 
         const speakOutput = makePostRequest('https://viacep.com.br/ws',cep)
